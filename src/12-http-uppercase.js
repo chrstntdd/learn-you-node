866bc7773b077120a0eb03c689ba3fea6715cfb5
Christian Todd <ChristianTodd27@icloud.com>
2017-02-03 15:08:34 -0500

    refactor and handle errors in exercise 12.

--- src/12-http-uppercase.js
+++ src/12-http-uppercase.js
@@ -3,14 +3,10 @@ let map = require('through2-map');
 let http = require('http');
 
 let server = http.createServer(function (req, res) {
-  if (req) {
-    req.pipe(map(function(chunk){
-        return chunk.toString().toUpperCase();
-    })).pipe(res)
+  if (req.method !== 'POST') {
+    return res.end('Please send a post request.');
   }
-});
+  req.pipe(map(chunk =>
+    chunk.toString().toUpperCase())).pipe(res);
 
-server.listen(port);
-
-//REFACTOR.
-//HANDLE ERRORS
+}).listen(port);
